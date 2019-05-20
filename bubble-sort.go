6f8502d454521cd3469185a202e658f68562a7e4
lihongtao <lihongtao@ainirobot.com>
2019-05-20 17:51:01 +0800

    add first version for bubble sort

---
+++ bubble-sort.go
@@ -0,0 +1,12 @@
+package sort
+
+func BubbleSort(data []Element, rule func(Element, Element) bool) []Element {
+    for i := 0;i < len(data);i++ {
+    	for j := i + 1; j < len(data);j++ {
+    		if rule(data[i],data[j]) {
+    			data[i],data[j] = data[j],data[i]
+			}
+		}
+	}
+	return data
+}
